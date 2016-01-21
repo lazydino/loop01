560932c9efb36ba4ed7d1613b667d5e215bdf779
Reggie Chen <reggie@magnetargames.com>
2016-01-21 13:49:31 -0800

    Fix sed script

--- postinstall.sh
+++ postinstall.sh
@@ -4,6 +4,4 @@ sed -i -r -e "s/(\"host\":).*/\1 \"$MYSQLD_PORT_3306_TCP_ADDR\",/" \
    -e "s/(\"username\":).*/\1 \"root\",/" \
    -e "s/(\"password\":).*/\1 \"$MYSQLD_ENV_MYSQL_ROOT_PASSWORD\",/" \
    /app/server/datasources.json
-sed -r "s/(masterIP *= *\").*/\1$hostIP\";/" mv/main.js.org > mv/main.js
-sed -r "s/(masterIP *= *\").*/\1$hostIP\";/" mv/Editor/main.js.org > mv/Editor/main.js
 
