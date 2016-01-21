4b8dee9eed098cbebb242633c86cae5f216bf927
Reggie Chen <reggie@magnetargames.com>
2016-01-21 13:52:35 -0800

    Correct path referenced in npm script

--- postinstall.sh
+++ postinstall.sh
@@ -3,5 +3,5 @@ sed -i -r -e "s/(\"host\":).*/\1 \"$MYSQLD_PORT_3306_TCP_ADDR\",/" \
    -e "s/(\"database\":).*/\1 \"matrixdb\",/" \
    -e "s/(\"username\":).*/\1 \"root\",/" \
    -e "s/(\"password\":).*/\1 \"$MYSQLD_ENV_MYSQL_ROOT_PASSWORD\",/" \
-   /app/server/datasources.json
+   server/datasources.json
 
