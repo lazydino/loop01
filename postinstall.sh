e046ee15a57876c60c9cbeb4ee10c35097552de4
Reggie Chen <reggie@magnetargames.com>
2016-01-22 11:35:44 -0800

    Run grunt after install.

--- postinstall.sh
+++ postinstall.sh
@@ -5,4 +5,6 @@ sed -i -r -e "0,/host/ s/(\"host\":).*/\1 \"$MYSQLD_PORT_3306_TCP_ADDR\",/" \
    -e "0,/password/ s/(\"password\":).*/\1 \"$MYSQLD_ENV_MYSQL_ROOT_PASSWORD\",/" \
    server/datasources.json
 #cat server/datasources.json
+grunt
+
 
