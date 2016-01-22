953aa3b3a1edce5bc68862c216164a38485babb5
Reggie Chen <reggie@magnetargames.com>
2016-01-21 22:37:15 -0800

    cat datasources for debug

--- postinstall.sh
+++ postinstall.sh
@@ -3,4 +3,5 @@ sed -i -r -e "0,/host/ s/(\"host\":).*/\1 \"$MYSQLD_PORT_3306_TCP_ADDR\",/" \
    -e "0,/database/ s/(\"database\":).*/\1 \"matrixdb\",/" \
    -e "0,/password/ s/(\"password\":).*/\1 \"$MYSQLD_ENV_MYSQL_ROOT_PASSWORD\",/" \
    server/datasources.json
+cat server/datasources.json
 
