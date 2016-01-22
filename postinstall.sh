55f905a5e9fec2c9a9edeb28c25b67935358fb83
Reggie Chen <reggie@magnetargames.com>
2016-01-21 22:44:16 -0800

    More debug output

--- postinstall.sh
+++ postinstall.sh
@@ -1,4 +1,5 @@
 # Replace datasources connector parameters before execution.
+echo "MYSQLD_PORT_3306_TCP_ADDR is "$MYSQLD_PORT_3306_TCP_ADDR
 sed -i -r -e "0,/host/ s/(\"host\":).*/\1 \"$MYSQLD_PORT_3306_TCP_ADDR\",/" \
    -e "0,/database/ s/(\"database\":).*/\1 \"matrixdb\",/" \
    -e "0,/password/ s/(\"password\":).*/\1 \"$MYSQLD_ENV_MYSQL_ROOT_PASSWORD\",/" \
