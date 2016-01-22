d2ccf5870c87e6aa4e99f45891b01a779763f05d
Reggie Chen <reggie@magnetargames.com>
2016-01-21 16:46:47 -0800

    Replace one occurence using sed.

--- postinstall.sh
+++ postinstall.sh
@@ -1,7 +1,6 @@
 # Replace datasources connector parameters before execution.
-#sed -i -r -e "s/(\"host\":).*/\1 \"$MYSQLD_PORT_3306_TCP_ADDR\",/" \
-#   -e "s/(\"database\":).*/\1 \"matrixdb\",/" \
-#   -e "s/(\"username\":).*/\1 \"root\",/" \
-#   -e "s/(\"password\":).*/\1 \"$MYSQLD_ENV_MYSQL_ROOT_PASSWORD\",/" \
-#   server/datasources.json
+sed -i -r -e "0,/host/ s/(\"host\":).*/\1 \"$MYSQLD_PORT_3306_TCP_ADDR\",/" \
+   -e "0,/database/ s/(\"database\":).*/\1 \"matrixdb\",/" \
+   -e "0,/password/ s/(\"password\":).*/\1 \"$MYSQLD_ENV_MYSQL_ROOT_PASSWORD\",/" \
+   server/datasources.json
 
